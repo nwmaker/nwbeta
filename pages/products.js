7c49c1a271f5515d7674d5a014b1bcc9ccb13293
nwmaker <makernw@gmail.com>
2018-02-09 10:45:09 -0700

    update-products

--- pages/products.js
+++ pages/products.js
@@ -1,9 +1,17 @@
 import Layout from '../comps/layout'
+import Link from 'next/link'
+
 //import Show from '../comps/photo-carousel/show'
 //import Slideshow from '../comps/slideshow'
 
 export default () => (
   <Layout>
-    <h1>NWMaker Porduct List</h1>
+    <div>
+      <h1>NWMaker Porduct List</h1>
+      <ul>
+        <Link href='/smd0201'><a>SMD 0201 Challenge</a></Link> {' '}
+        <Link href='/ursaminor'><a>Ursa Minor Bear</a></Link>
+      </ul>
+    </div>
   </Layout>
 )
