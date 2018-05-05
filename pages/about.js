0b071d14c90470eaf07046a3a4163c8986f56961
nwmaker <makernw@gmail.com>
2018-05-04 21:07:00 -0600

    update-about

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,27 @@
+import React from 'react'
+import { Column, Row } from 'simple-flexbox'
 import Layout from '../comps/layout'
 
 export default () => (
   <Layout>
-    <h1>NWMaker is a design studio on electronics craft.</h1>
+    <Column flexGrow={1}>
+      <Row horizontal='center'>
+        <h1>A design studio on electronics crafts</h1>
+      </Row>
+      <Row vertical='center'>
+        <Column flexGrow={1} horizontal='center'>
+          <h2> Hardware </h2>
+          <span> Customized PCB design </span>
+        </Column>
+        <Column flexGrow={1} horizontal='center'>
+          <h2> Software </h2>
+          <span> Embedded, mobile/web programming </span>
+        </Column>
+        <Column flexGrow={1} horizontal='center'>
+          <h2> System </h2>
+          <span> From concept to product </span>
+        </Column>
+      </Row>
+    </Column>
   </Layout>
 )
